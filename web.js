fb82242280fabfd8eaf7e834bd8159ea49132dba
Mayec Rancel <example@stanford.edu>
2013-07-27 21:52:31 +0000

    debug

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html');
-  Response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
